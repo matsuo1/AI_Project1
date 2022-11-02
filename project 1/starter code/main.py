55f132aa0af81853c3d34a2c7199e092c30c23d8
Kaz Matsuo <kazmat44@Kazs-MacBook-Air.local>
2022-11-01 19:30:29 -0500

    woah

--- project 1/starter code/main.py
+++ project 1/starter code/main.py
@@ -6,6 +6,7 @@
 ###=================================================
 #testing
 from state import State
+import itertools
 
 class Plan:
     def __init__(self, initial_state, goal_state):
@@ -129,6 +130,98 @@ class Plan:
             print("possible moves: ", possibleMoves)
         return possibleMoves
    
+    def newneighbors(self,currentstate):
+        #Make 5 lists: putdown, unstack, stack, pickup, move
+        putdown = []
+        unstack =[]
+        stack = []
+        pickup = []
+        move =[]
+        #Make a copy of the current state
+        curr = currentstate.deepcopy()
+
+        #Make a table with the initial_state
+
+        #blocks in the air list
+        #loop through each block in the state and check whether the block is in the air
+        #Add those blocks into the list
+        air =[]
+        for items in curr:
+            if items.air == True:
+                air.append(items)
+
+        #blocks on the table and clear list
+        #loop through each block in the state and check the appropriate conditions for the blocks to be on table and clear
+        #Add those blocks into the list
+        ontable_clear = []
+        for items in curr:
+            if items.on =='table' and items.clear == True:
+                ontable_clear.append(items)
+            
+
+        #blocks that are clear and not on the table
+        #loop through each block in the state and check the appropriate conditions for
+        #the blocks to be not on table and clear
+        #Add those blocks into the list
+        nottable_clear = []
+        for items in curr:
+            if items.on !='table' and items.clear == True:
+                nottable_clear.append(items)
+
+        #if block in air list have element(s) then only do putdown and stack
+        if air:
+            #putdown
+            #copy the state
+            tryputdown = curr.deepcopy()#Maybe not curr
+            for items in tryputdown:#get the equivalent block from the copied state
+                if items == air[0]:
+                    self.putdown(items)#apply the operator
+            print(air)
+            putdown.append(tryputdown) #add those into putdown list (made in the previous step)
+
+            #stack
+            clear = ontable_clear + nottable_clear
+                #it is a combination of (blocks_air & blocks_clear) (itertool recommendation)
+                #loop through all combinations:
+            stackblockproduct = itertools.product(air[0],clear)
+            print(stackblockproduct)
+            for block,block1 in stackblockproduct:
+                trystack = curr.deepcopy()
+                    #make a copy 
+                # Figure out the Ids for block and block 1 using find
+                block_id = State.find(trystack, block)
+                block1_id = State.find(trystack, block1)
+                self.stack(block_id,block1_id) 
+                stack.append(trystack, f"stack{block_id, block1_id}")  
+                    #get the equivalent block from the copied state
+                    #apply operator for the two blocks in the operator
+                    #add those into stack list (made in the previous step)
+        else:
+    #if the block is not in the air - options: pickup, unstack, move (the steps for these should be somewhat the same as putdown and stack)
+        #pickup options
+            for items in ontable_clear:
+                trypick = curr.copy()
+                for stuff in trypick:
+                    if items == stuff:
+                        self.pickup(stuff)
+                        break
+                pickup.append(trypick)
+
+            #apply pick operator on all blocks that are clear and are on the table
+            #Add those into the pick list (made in the previous step)
+        #unstack
+            for items in nottable_clear:
+                tryunstack = curr.copy()
+                
+            #apply unstack operator to all blocks that are clear and are not on table
+            #Add those into the unstack list (made in the previous step)
+        #move
+
+            #apply move operator to all bloakcs that are clear and not on the table to any block that is clear
+            #it is the combination of blocks that are clear and not on table with blocks are simply clear (itertools recommendation)
+
+    #return all five lists
+        return pickup,putdown,stack,unstack,move
 
     def clearBlocks(self,state):
         openMoves = []
@@ -139,11 +232,14 @@ class Plan:
         return openMoves
 
     def heuristic(self,moves,goal_state_blocks,initial_state_blocks): #pick-up function 
+        totalnow = 0
+        target = 0
         blocks = [] # can anyone think of a better/ more efficent way to get the highest #
         for i in range(len(initial_state_blocks)):
             score =0 
             if i == 0:
                 continue
+            target += 2
             if initial_state_blocks[i].clear == goal_state_blocks[i].clear:
                 score += 1
                 #print("CLEAR",initial_state_blocks[i],initial_state_blocks[i].clear)
@@ -152,14 +248,16 @@ class Plan:
                 score += 1
                 #print("ON",initial_state_blocks[i],initial_state_blocks[i].on)
             #print("what it should be: ", goal_state_blocks[i],goal_state_blocks[i].on)
+            totalnow += score
             if initial_state_blocks[i] in moves:
-                blocks.append((score,initial_state_blocks[i]))
-            print("the score for ", initial_state_blocks[i], "is ", score)
-            print("blocks list consists of ", blocks)
-            print(initial_state_blocks[i], "is clear?", initial_state_blocks[i].clear, "and is on?", initial_state_blocks[i].on)
+                blocks.append((score,initial_state_blocks[i].id,initial_state_blocks[i]))
+            #print("the score for ", initial_state_blocks[i], "is ", score)
+            #print("blocks list consists of ", blocks)
+            #print(initial_state_blocks[i], "is clear?", initial_state_blocks[i].clear, "and is on?", initial_state_blocks[i].on)
         #blocks.sort()
-        bestMove = blocks.pop(0)
-        print("thebest move,", bestMove)
+        print(totalnow, "vs", target)
+        # bestMove = blocks.pop(0)
+        # print("thebest move,", bestMove)
         return blocks
     
     def putdown_priority(self,block1,current_state,goal_state_blocks): 
@@ -188,17 +286,19 @@ class Plan:
         moves = self.clearBlocks(initial_state)
         print("SOS",moves)
         frontier = self.heuristic(moves,goal,initial_state_blocks)  #what block, from the list of the clear blocks should we move? 
-        print ("frontier ", frontier)
+        print ("frontieraf ", frontier)
          # ***=========================================
         #Avistited.append(start) 
         while(frontier):
-            #frontier.sort()
-            bestBlock = frontier.pop() # node with the lowest ASTAR score in frontier.
-            print (bestBlock[1])
+            frontier = self.heuristic(moves,goal,initial_state_blocks)
+            frontier.sort()
+            print("yuh", frontier)
+            bestBlock = frontier.pop(0) # node with the lowest ASTAR score in frontier.
+            print (bestBlock[2])
            # path.append(node[2])
             #if node[2] == end:
              #   return path #retrun path       
-            nextneighbors = self.neighbors(bestBlock[1],currState) 
+            nextneighbors = self.neighbors(bestBlock[2],currState) 
             print("neighbors", nextneighbors)  
             #moveTo = self.putdown_priority(aScore1,currState,goal) #need
             #print("move to", moveTo[1])
@@ -210,7 +310,8 @@ class Plan:
                 #    frontier.append((aScore,pathCost,neighbor))
                  #   Avistited.append(neighbor)
             print("This it?", frontier)
-            self.putdown_priority(bestBlock[1],currState,goal)
+            self.putdown_priority(bestBlock[2],currState,goal)
+            #break
         return 0
 
 
@@ -224,7 +325,7 @@ class Plan:
         # You must automate this code such that it would produce a plan for any initial and goal states.
 
         block_c = State.find(self.initial_state, "C")
-        #block_d = State.find(self.initial_state, "D")
+        block_d = State.find(self.initial_state, "D")
         #block_e = State.find(self.initial_state, "E")
   
         self.aStar(self.initial_state, goal_state_blocks)
@@ -240,7 +341,7 @@ class Plan:
         #self.unstack(block_d, block_c)
 
         #print the state
-        #action = f"unstack{block_d, block_c}"
+        action = f"unstack{block_d, block_c}"
         #State.display(self.initial_state, message=action)
 
         #put the block on the table
